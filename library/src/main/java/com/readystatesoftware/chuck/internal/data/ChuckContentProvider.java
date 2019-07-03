50003127cdc1eae723a782873f4576e2aa8f534b
kgoel <kgoel@microstrategy.com>
2019-07-03 12:38:28 -0400

    moved to androidx

--- library/src/main/java/com/readystatesoftware/chuck/internal/data/ChuckContentProvider.java
+++ library/src/main/java/com/readystatesoftware/chuck/internal/data/ChuckContentProvider.java
@@ -24,9 +24,9 @@ import android.content.pm.ProviderInfo;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.annotation.WorkerThread;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.WorkerThread;
 
 import com.readystatesoftware.chuck.internal.support.JsonConvertor;
 
